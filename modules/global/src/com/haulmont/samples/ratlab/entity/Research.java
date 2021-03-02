a29069f08fc3016d3aa055bc836fcd46419be727
Dmitry Taimanov <taimanov@haulmont.com>
2021-03-02 15:54:53 +0400

    Project and base entities created

---
+++ modules/global/src/com/haulmont/samples/ratlab/entity/Research.java
@@ -0,0 +1,71 @@
+package com.haulmont.samples.ratlab.entity;
+
+import com.haulmont.chile.core.annotations.Composition;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.OnDelete;
+import com.haulmont.cuba.core.entity.annotation.OnDeleteInverse;
+import com.haulmont.cuba.core.global.DeletePolicy;
+import com.haulmont.samples.ratlab.entity.stuff.Employee;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Table(name = "RATLAB_RESEARCH")
+@Entity(name = "ratlab_Research")
+@NamePattern("%s|title")
+public class Research extends StandardEntity {
+    private static final long serialVersionUID = 3593240352594836892L;
+
+    @Column(name = "TITLE", nullable = false)
+    @NotNull
+    private String title;
+
+    @Composition
+    @OnDeleteInverse(DeletePolicy.UNLINK)
+    @OnDelete(DeletePolicy.UNLINK)
+    @OneToMany(mappedBy = "research")
+    private List<Employee> participants;
+
+    @NotNull
+    @Column(name = "START_TIME", nullable = false)
+    private OffsetDateTime startTime;
+
+    @Lob
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public OffsetDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(OffsetDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public List<Employee> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<Employee> participants) {
+        this.participants = participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ modules/global/src/com/haulmont/samples/ratlab/entity/resources/MiscResource.java
@@ -0,0 +1,51 @@
+package com.haulmont.samples.ratlab.entity.resources;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Table(name = "RATLAB_MISC_RESOURCE")
+@Entity(name = "ratlab_MiscResource")
+@NamePattern("%s|name")
+public class MiscResource extends StandardEntity {
+    private static final long serialVersionUID = -1707059619304141296L;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AMOUNT")
+    private BigDecimal amount;
+
+    @NotNull
+    @Column(name = "TYPE_", nullable = false)
+    private String type;
+
+    public ResourceType getType() {
+        return type == null ? null : ResourceType.fromId(type);
+    }
+
+    public void setType(ResourceType type) {
+        this.type = type == null ? null : type.getId();
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
